24b8c2afdb40573719060ca350030d0f158da74f
Aske21 <veledarevica@yahoo.com>
2024-05-20 01:52:17 +0200

    fix(*): Header responsiveness

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -19,28 +19,17 @@ const Header = () => {
     { name: t("products"), path: "/products" },
     { name: t("about"), path: "/about" },
     { name: t("contact"), path: "/contact" },
-    { name: <FaUser />, path: "/login" }, // Login icon
+    { name: <FaUser />, path: "/login" },
     { name: t("cart"), path: "/cart" },
   ];
 
   return (
-    <div className="relative z-10 border-b py-4">
+    <header className="sticky top-0 z-50 shadow py-4 bg-background border-b">
       <div className="container mx-auto flex justify-between items-center">
         <NextLink href="/" locale={locale}>
           <span>ShoppyDev</span>
         </NextLink>
-        <div className="md:hidden">
-          {isOpen ? (
-            <FaTimes className="text-gray-600" onClick={onToggle} />
-          ) : (
-            <FaBars className="text-gray-600" onClick={onToggle} />
-          )}
-        </div>
-        <nav
-          className={`md:flex md:space-x-4 items-center ${
-            isOpen ? "block" : "hidden"
-          } `}
-        >
+        <nav className="hidden md:flex md:space-x-4 items-center">
           {routes.map((route, index) => (
             <NextLink
               key={index}
@@ -70,8 +59,61 @@ const Header = () => {
             </NextLink>
           ))}
         </nav>
+        <div className="md:hidden">
+          {isOpen ? (
+            <FaTimes className="text-gray-600" onClick={onToggle} />
+          ) : (
+            <FaBars
+              className="text-gray-600"
+              onClick={() => {
+                onToggle();
+              }}
+            />
+          )}
+        </div>
+        {isOpen && (
+          <nav className="md:hidden absolute top-full left-0 w-full z-50 bg-background border-t">
+            <ul className="flex flex-col items-center">
+              {routes.map((route, index) => (
+                <li key={index} className="py-2">
+                  <NextLink
+                    href={`${route.path}`}
+                    locale={locale}
+                    onClick={(e) =>
+                      route.path === pathname && e.preventDefault()
+                    }
+                    className={`${
+                      `/${locale}${route.path}` === pathname
+                        ? "underline decoration-slate-900 decoration-2 underline-offset-4"
+                        : ""
+                    }`}
+                  >
+                    <span>
+                      <div className="relative">
+                        <div className="relative py-2">
+                          <div className="t-0 absolute left-3">
+                            {route.name === t("cart") && totalCartItems > 0 && (
+                              <p className="flex h-0.5 w-0.5 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">
+                                {totalCartItems}{" "}
+                              </p>
+                            )}
+                          </div>
+                          {route.name !== t("cart") ? (
+                            route.name
+                          ) : (
+                            <FaShoppingCart />
+                          )}
+                        </div>
+                      </div>
+                    </span>
+                  </NextLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
       </div>
-    </div>
+    </header>
   );
 };
 
